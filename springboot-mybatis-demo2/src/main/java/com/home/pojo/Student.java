8ef96e80b14bb7a60387442366669ade780073cc
niepu1991 <niepu1991@163.com>
2018-08-25 23:06:06 +0800

    springboot-mybatis的crud小案例

---
+++ springboot-mybatis-demo2/src/main/java/com/home/pojo/Student.java
@@ -0,0 +1,80 @@
+package com.home.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public class Student {
+    private int id;
+    private String name;
+
+    private int age;
+    private String attr;
+
+    private Date createtime;
+    private Date updatetime;
+
+    public Student(){}
+    public Student(Integer id,String name,int age,String attr){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.attr=attr;
+    }
+    public Student(Integer id,String name){
+        this.id=id;
+        this.name=name;
+    }
+    public Student(Integer id){
+        this.id=id;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAttr() {
+        return attr;
+    }
+
+    public void setAttr(String attr) {
+        this.attr = attr;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+}
