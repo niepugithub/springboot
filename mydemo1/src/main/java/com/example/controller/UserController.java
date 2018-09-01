ad544dcb55aac0518a2334b6e2dbe3e645345d31
niepu1991 <niepu1991@163.com>
2018-09-01 19:20:45 +0800

    使用log4j2日志

--- mydemo1/src/main/java/com/example/controller/UserController.java
+++ mydemo1/src/main/java/com/example/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import com.example.beans.User;
 import com.example.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping("getAllUser")
     @ResponseBody
     public List<User> getAllUser(){
+        LOGGER.trace("======================打印trace日志===================");
         return userService.getAllUser();
     }
 }
