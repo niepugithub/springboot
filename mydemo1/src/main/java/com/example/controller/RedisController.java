69d0a872b89828de73979b4b2749fb34b68ebc1a
niepu1991 <niepu1991@163.com>
2018-09-02 10:02:49 +0800

    加入jedis依赖，测试jedis.set OK

---
+++ mydemo1/src/main/java/com/example/controller/RedisController.java
@@ -0,0 +1,11 @@
+package com.example.controller;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisController {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("192.168.197.129",6379);
+        System.out.println("jedis========="+jedis);
+        jedis.set("jedis","第一个jedis值");
+    }
+}
