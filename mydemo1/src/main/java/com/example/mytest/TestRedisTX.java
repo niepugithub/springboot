b89d0b6f0631b99577191095af5a29221cf8d17e
niepu1991 <niepu1991@163.com>
2018-09-02 11:55:26 +0800

    模拟转账事务,有待研究

--- mydemo1/src/main/java/com/example/mytest/TestRedisTX.java
+++ mydemo1/src/main/java/com/example/mytest/TestRedisTX.java
@@ -5,6 +5,10 @@ import redis.clients.jedis.Transaction;
 
 public class TestRedisTX {
     public static void main(String[] args) {
+        System.out.println(complexTx());
+    }
+
+    public static void simpleTx(){
         Jedis jedis= new Jedis("192.168.197.129",6379);
         Transaction tx = jedis.multi();
         // 注意这里用tx设值，而不是jedis，否则报错edisDataException
@@ -12,5 +16,34 @@ public class TestRedisTX {
         tx.set("k2","v22");
         tx.set("k3","v33");
         tx.exec();
+    }
+
+    public static boolean complexTx(){
+        Jedis jedis= new Jedis("192.168.197.129",6379);
+        int amtToSubtract=10;
+        jedis.watch("balance");
+        // 其他程序修改了balance
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        int balance = Integer.parseInt(jedis.get("balance"));
+        System.out.println(balance);
+        if(balance<amtToSubtract){
+            jedis.unwatch();
+            System.out.println("==========modify by someone=========");
+            return false;
+        }else {
+            System.out.println("=====transaction====");
+            Transaction tx = jedis.multi();
+            tx.decrBy("balance",amtToSubtract);
+
+            tx.incrBy("debt",amtToSubtract);
+            tx.exec();
+            System.out.println("balance====="+jedis.get("balance"));
+            System.out.println("debt====="+jedis.get("debt"));
+            return true;
+        }
     }
 }
