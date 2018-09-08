bd7c768e701776947e2847ccf549353527254011
niepu1991 <niepu1991@163.com>
2018-09-08 09:40:19 +0800

    testRedisMS的一些修改

--- mydemo1/src/main/java/com/example/mytest/TestRedisMS.java
+++ mydemo1/src/main/java/com/example/mytest/TestRedisMS.java
@@ -4,12 +4,12 @@ import redis.clients.jedis.Jedis;
 
 public class TestRedisMS {
     public static void main(String[] args) {
-        Jedis jedis_m = new Jedis("192.168.197.129",6380);
-        // System.out.println(jedis_m.ping()); // 判断是否能够连接上 pong表示连接上
-        // System.out.println(jedis_m.isConnected());
-        jedis_m.set("k1","总算成功了");
-        Jedis jedis_s = new Jedis("192.168.197.129",6381);
-//        jedis_s.slaveof("192.168.197.129",6380);
-        System.out.println(jedis_s.get("k1"));
+        Jedis jedis_m = new Jedis("192.168.197.129",6379);
+        System.out.println(jedis_m.ping()); // 判断是否能够连接上 pong表示连接上
+        System.out.println(jedis_m.isConnected());
+        jedis_m.set("k6","再来一次,OK?");
+        Jedis jedis_s = new Jedis("192.168.197.129",6380);
+        jedis_s.slaveof("192.168.197.129",6379);
+        System.out.println(jedis_s.get("k6"));
     }
 }
