ee982fd63fa6b54d24178e56193a67940f8a50cd
niepu1991 <niepu1991@163.com>
2018-09-15 22:19:04 +0800

    rabbitmq-simple-加上consumer

--- itcast-rabbitmq/src/main/java/com/example/rabbitmq/simple/Recv.java
+++ itcast-rabbitmq/src/main/java/com/example/rabbitmq/simple/Recv.java
@@ -1,5 +1,46 @@
 package com.example.rabbitmq.simple;
 
-public class Recv {
+import com.example.rabbitmq.util.ConnectionUtil;
+import com.rabbitmq.client.*;
 
+import java.io.IOException;
+
+public class Recv {
+    private final static String QUEUE_NAME = "test_queue";
+
+    public static void main(String[] args) throws Exception {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println(" [x] Done");
+                }
+            }
+        };
+        boolean autoAck = true; // acknowledgment is covered below
+        channel.basicConsume(QUEUE_NAME, autoAck, consumer);
+
+        channel.close();
+        connection.close();
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
